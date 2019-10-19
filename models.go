8b26c1636967621681248316510d0d0dab778747
sp <s.pedrett@pitw.ch>
2019-10-19 14:57:01 +0200

    Fix: Fix Models

--- models.go
+++ models.go
@@ -1,16 +1,19 @@
 package subscriptionparser
 
+// Subscriptions provides meta data for subscriptions
 type Subscriptions struct {
 	Subscriber string
 	Products   []Product
 }
 
+// Product provides meta data for products
 type Product struct {
 	Menge          int
 	Artikel        Article
 	EndkundenPreis float64
 }
 
+// Article provides meta data for each article
 type Article struct {
 	ArticleNr   string
 	Description string
